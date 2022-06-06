30c5e77d99c9122a6d0531a16b7cd7b66e7225d7
Bolololo91 <karolczarnecki5@gmail.com>
2022-06-06 11:51:29 +0200

    Dodaj pliki projektów.

---
+++ Array2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            char[] BoloLolo = text.ToArray();
+            string newBoloLolo = "";
+            for (int i = text.Length-1; i >-1; i--)
+            {
+                newBoloLolo += BoloLolo[i];
+            }
+            Console.WriteLine(newBoloLolo);
+            Console.ReadLine();
+
+        }
+    }
+}
